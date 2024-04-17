d535908fc8955e46a2cdbbd90dfd410fbd8e0363
sspatnaik <167024541+sspatnaik@users.noreply.github.com>
2024-04-17 21:32:35 +0530

    Update spa_sql.ps1

--- spa_sql.ps1
+++ spa_sql.ps1
@@ -1,18 +1,5 @@
 param (
-    [string]$ServerInstance,
-    [string]$Database,
-    [string]$Username,
-    [string]$Password
-)
+    [string]$ServerInstance
+    )
 Write-Output "SPA Server Instance Name"
 Write-Output $ServerInstance
-
-Write-Output "SPA Database Name"
-Write-Output $Database
-
-Write-Output "SPA User Name"
-Write-Output $Username
-
-Write-Output "SPA Password"
-Write-Output $Password
-
