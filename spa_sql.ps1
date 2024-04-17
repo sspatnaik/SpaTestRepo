7bee724a72dd844d24ce731b4912f71a7e9012cf
sspatnaik <167024541+sspatnaik@users.noreply.github.com>
2024-04-17 20:28:21 +0530

    Update spa_sql.ps1

--- spa_sql.ps1
+++ spa_sql.ps1
@@ -4,6 +4,17 @@ param (
     [string]$Username,
     [string]$Password
 )
+Write-Output "SPA Server Instance Name"
+Write-Output $ServerInstance
+
+Write-Output "SPA Database Name"
+Write-Output $Database
+
+Write-Output "SPA User Name"
+Write-Output $Username
+
+Write-Output "SPA Password"
+Write-Output $Password
 
 Install-Module -Name SqlServer -Scope CurrentUser -Force
 
